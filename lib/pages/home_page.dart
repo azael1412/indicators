17e0bad9c1008b5de5de728db0a854ca72ea3413
Azael Espinosa Sánchez <azael_1412@hotmail.com>
2020-07-25 17:16:27 -0500

    se modificaron algunos valores enteros a doble

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,148 +9,151 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-   PageController _pageController;
+  PageController _pageController;
   int currentIndex = 0;
   @override
-  void initState() { 
-    _pageController = PageController(
-      initialPage: 0
-    );
+  void initState() {
+    _pageController = PageController(initialPage: 0);
     super.initState();
   }
+
   @override
   void dispose() {
     _pageController.dispose();
     // TODO: implement dispose
     super.dispose();
   }
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 20, top: 20),
-            child: Text('Skip', style: TextStyle(
-              color: ColorSys.primary,
-              fontSize: 18,
-              fontWeight: FontWeight.w600
-            ),),
-          )
-        ]),
-        body: Stack(
-          alignment: Alignment.bottomCenter,
-          children: <Widget>[
-            PageView(
-              onPageChanged: (int page) {
+      appBar:
+          AppBar(elevation: 0, backgroundColor: Colors.white, actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 20, top: 20),
+          child: Text(
+            'Skip',
+            style: TextStyle(
+                color: ColorSys.primary,
+                fontSize: 18,
+                fontWeight: FontWeight.w600),
+          ),
+        )
+      ]),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          PageView(
+            onPageChanged: (int page) {
               setState(() {
                 currentIndex = page;
               });
             },
-              controller: _pageController,
-              children: <Widget>[
-                makePage(
+            controller: _pageController,
+            children: <Widget>[
+              makePage(
                   image: 'assets/img/step-one.png',
                   title: Strings.stepOneTitle,
-                  content: Strings.stepOneContent
-                ),
-                 makePage(
+                  content: Strings.stepOneContent),
+              makePage(
                   reverse: true,
                   image: 'assets/img/step-two.png',
                   title: Strings.stepTwoTitle,
-                  content: Strings.stepTwoContent
-                ),
-                 makePage(
+                  content: Strings.stepTwoContent),
+              makePage(
                   image: 'assets/img/step-three.png',
                   title: Strings.stepThreeTitle,
-                  content: Strings.stepThreeContent
-                ),
-              ],
+                  content: Strings.stepThreeContent),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 60.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _buildIndicator(),
             ),
-            Container(
-              margin: EdgeInsets.only(bottom: 60),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: _buildIndicator(),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
+      ),
     );
   }
-Widget makePage({image, title, content, reverse = false}) {
+
+  Widget makePage({image, title, content, reverse = false}) {
     return Container(
       padding: EdgeInsets.only(left: 50, right: 50, bottom: 60),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-
-          !reverse ? 
-          Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Image.asset(image),
-              ),
-              SizedBox(height: 30,),
-            ],
-          ) : SizedBox(),
-
-          Text(title, style: TextStyle(
-            color: ColorSys.primary,
-            fontSize: 30,
-            fontWeight: FontWeight.bold
-          ),),
-
-          SizedBox(height: 20,),
-          Text(content, textAlign: TextAlign.center, style: TextStyle(
-            color: ColorSys.gray,
-            fontSize: 20,
-            fontWeight: FontWeight.w400
-          ),),
-
-          reverse ? 
-          Column(
-            children: <Widget>[
-              SizedBox(height: 30,),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Image.asset(image),
-              ),
-            ],
-          ) : SizedBox(),
-
+          !reverse
+              ? Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Image.asset(image),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                )
+              : SizedBox(),
+          Text(
+            title,
+            style: TextStyle(
+                color: ColorSys.primary,
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            content,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: ColorSys.gray,
+                fontSize: 20,
+                fontWeight: FontWeight.w400),
+          ),
+          reverse
+              ? Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Image.asset(image),
+                    ),
+                  ],
+                )
+              : SizedBox(),
         ],
       ),
     );
   }
-    Widget _indicator(bool isActive) {
+
+  Widget _indicator(bool isActive) {
     return AnimatedContainer(
       duration: Duration(milliseconds: 300),
-      height: 6,
-      width: isActive ? 30 : 6,
-      margin: EdgeInsets.only(right: 5),
+      height: 6.0,
+      width: isActive ? 30.0 : 6.0,
+      margin: EdgeInsets.only(right: 5.5),
       decoration: BoxDecoration(
-        color: ColorSys.secoundry,
-        borderRadius: BorderRadius.circular(5)
-      ),
+          color: ColorSys.secoundry, borderRadius: BorderRadius.circular(5)),
     );
   }
 
   List<Widget> _buildIndicator() {
     List<Widget> indicators = [];
-    for (int i = 0; i<3; i++) {
+    for (int i = 0; i < 3; i++) {
       if (currentIndex == i) {
         indicators.add(_indicator(true));
       } else {
         indicators.add(_indicator(false));
       }
     }
-
     return indicators;
   }
 }
